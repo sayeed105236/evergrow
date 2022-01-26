120d8b112fd440307ebbc543957802f3783d6130
Masikur Rahman <misuk@businessaccelerate.com.bd>
2022-01-26 12:49:14 +0600

    fix left right active

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,44 +32,7 @@ class HomeController extends Controller
     {
         return view('home');
     }
-    public function binary_count($placement_id,$pos)
-    {
-      //dd($placement_id,$pos);
-       if ($pos == 1){
-            $pos = 'left_count';
-       }else{
-           $pos = 'right_count';
-       }
 
-        while($placement_id != '' && $pos != ''){
-
-            DB::statement("UPDATE users SET $pos = `$pos`+1 WHERE user_name = '$placement_id'");
-
-            //$this->is_pair_generate($placement_id);
-            $pos= $this->find_position_id($placement_id);
-            $placement_id= $this->find_placement_id($placement_id);
-
-        }
-
-    }
-    public function find_position_id($placement_id){
-
-            $user_id = User::where('user_name',$placement_id)->first();
-            $pos= $user_id->position;
-            if ($pos == 1){
-                $pos = 'left_count';
-            }elseif($pos == 2){
-                $pos = 'right_count';
-            }
-
-            return $pos;
-
-    }
-    public function find_placement_id($placement_id){
-
-            $user_id = User::where('user_name',$placement_id)->first();
-            return $user_id->placement_id;
-    }
     public function Activate($id)
     {
 
@@ -127,6 +90,48 @@ class HomeController extends Controller
       $sponsor_bonus->save();
 
       return back()->with('package_activated','Congratulations!! Your Package is Now Activated');
+    }
+
+    public function binary_count($placement_id,$pos)
+    {
+        //dd($placement_id,$pos);
+        if ($pos == 1){
+            $pos = 'left_count';
+            $pos_ac = 'left_active';
+        }else{
+            $pos = 'right_count';
+            $pos_ac = 'right_active';
+        }
+
+        while($placement_id != '' && $pos != ''){
+
+            DB::statement("UPDATE users SET $pos = `$pos`+1 WHERE user_name = '$placement_id'");
+            DB::statement("UPDATE users SET $pos_ac = `$pos_ac`+1 WHERE user_name = '$placement_id'");
+
+            //$this->is_pair_generate($placement_id);
+            $pos= $this->find_position_id($placement_id);
+            $placement_id= $this->find_placement_id($placement_id);
+
+        }
+
+    }
+    public function find_position_id($placement_id){
+
+        $user_id = User::where('user_name',$placement_id)->first();
+        $pos= $user_id->position;
+        if ($pos == 1){
+            $pos = 'left_count';
+        }elseif($pos == 2){
+            $pos = 'right_count';
+        }
+
+        return $pos;
+
+    }
+    public function find_placement_id($placement_id){
+
+        $user_id = User::where('user_name',$placement_id)->first();
+        return $user_id->placement_id;
     }
 
 }
