fe065549a8d1a1eef691a3d3a1155a5c6dabb5b5
Abu Sayeed <abusayeed33343536@gmail.com>
2022-04-08 02:27:51 +0600

    sc run2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->command('pairbonus:daily')
-            ->daily();
+            ->everyMinute();
     }
 
     /**
